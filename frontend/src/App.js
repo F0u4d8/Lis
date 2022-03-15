38ecb2b895193ed586e8eca44af45d87f03b2d5b
F0u4d8 <f0u4d08@gmail.com>
2022-03-15 18:14:38 +0100

    Lis1

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main>List Products</main>
+      <footer>All Rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
